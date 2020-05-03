b01ba08b068356c91b8e8bb6163f32b3ce0a1d3f
coderus <coderusinbox@gmail.com>
2020-05-03 19:07:50 +0300

    [effects] Added Flock effect Ported from https://community.alexgyver.ru/threads/wifi-lampa-budilnik-obsuzhdenie-proshivki-ot-gunner47.2418/page-72#post-33652

--- src/EffectsManager.cpp
+++ src/EffectsManager.cpp
@@ -57,6 +57,7 @@
 #include "effects/basic/WaterfallPaletteEffect.h"
 #include "effects/basic/RainEffect.h"
 #include "effects/basic/PrismataEffect.h"
+#include "effects/basic/FlockEffect.h"
 
 #include <map>
 
@@ -267,4 +268,5 @@ EffectsManager::EffectsManager()
     effectsMap[PSTR("WaterfallPaletteEffect")] = new WaterfallPaletteEffect();
     effectsMap[PSTR("RainEffect")] = new RainEffect();
     effectsMap[PSTR("PrismataEffect")] = new PrismataEffect();
+    effectsMap[PSTR("FlockEffect")] = new FlockEffect();
 }
---
+++ src/effects/basic/FlockEffect.cpp
@@ -0,0 +1,122 @@
+#include "FlockEffect.h"
+
+namespace {
+
+Boid* boids = nullptr;
+
+uint8_t availableBoidCount = 20;
+
+Boid *predator = nullptr;
+PVector *wind = nullptr;
+
+uint8_t boidCount = 10;
+bool predatorPresent = true;
+
+bool havePredator = false;
+
+uint8_t hue = 0;
+
+}
+
+FlockEffect::FlockEffect()
+{
+
+}
+
+void FlockEffect::activate()
+{
+    wind = new PVector;
+    boids = new Boid[availableBoidCount];
+
+    for (int i = 0; i < boidCount; i++) {
+        boids[i] = Boid(15, 15);
+        boids[i].maxspeed = 0.380 * settings.speed / 127.0 + 0.380 / 2;
+        boids[i].maxforce = 0.015 * settings.speed / 127.0 + 0.015 / 2;
+    }
+    predatorPresent = havePredator && random(0, 2) >= 1;
+    if (predatorPresent) {
+        predator = new Boid(31, 31);
+        predator->maxspeed = 0.385 * settings.speed / 127.0 + 0.385 / 2;
+        predator->maxforce = 0.020 * settings.speed / 127.0 + 0.020 / 2;
+        predator->neighbordist = 8.0;
+        predator->desiredseparation = 0.0;
+    }
+}
+
+void FlockEffect::deactivate()
+{
+    delete wind;
+    delete[] boids;
+    if (predator) {
+        delete predator;
+    }
+}
+
+void FlockEffect::tick()
+{
+    myMatrix->applyBlur2d(15);
+    myMatrix->dimAll(255 - (settings.scale % 11) * 3);
+
+    bool applyWind = random(0, 255) > 240;
+    if (applyWind) {
+        wind->x = Boid::randomf() * 0.015f * settings.speed / 127.0f + 0.01f / 2;
+        wind->y = Boid::randomf() * 0.015f * settings.speed / 127.0f + 0.015f / 2;
+    }
+
+    CRGB color = ColorFromPalette(*myMatrix->GetPalette(settings.scale), hue);
+
+
+    for (int i = 0; i < boidCount; i++) {
+        Boid * boid = &boids[i];
+
+        if (predatorPresent) {
+            // flee from predator
+            boid->repelForce(predator->location, 10);
+        }
+
+        boid->run(boids, boidCount);
+        boid->wrapAroundBorders();
+        PVector location = boid->location;
+        // PVector velocity = boid->velocity;
+        // backgroundLayer.drawLine(location.x, location.y, location.x - velocity.x, location.y - velocity.y, color);
+        // effects.leds[XY(location.x, location.y)] += color;
+        myMatrix->drawPixelXY(location.x, location.y, color);
+
+        if (applyWind) {
+            boid->applyForce(*wind);
+            applyWind = false;
+        }
+    }
+
+    if (predatorPresent) {
+        predator->run(boids, boidCount);
+        predator->wrapAroundBorders();
+        color = ColorFromPalette(*myMatrix->GetPalette(settings.scale), hue + 128);
+        PVector location = predator->location;
+        // PVector velocity = predator.velocity;
+        // backgroundLayer.drawLine(location.x, location.y, location.x - velocity.x, location.y - velocity.y, color);
+        // effects.leds[XY(location.x, location.y)] += color;
+        myMatrix->drawPixelXY(location.x, location.y, color);
+    }
+
+    EVERY_N_MILLIS(333) {
+        hue++;
+    }
+
+    EVERY_N_SECONDS(30) {
+        predatorPresent = havePredator && !predatorPresent;
+    }
+}
+
+void FlockEffect::initialize(const JsonObject &json)
+{
+    Effect::initialize(json);
+    if (json.containsKey(F("havePredator"))) {
+        havePredator = json[F("havePredator")];
+    }
+}
+
+void FlockEffect::writeSettings(JsonObject &json)
+{
+    json[F("havePredator")] = havePredator;
+}
