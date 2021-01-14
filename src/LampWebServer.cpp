53bab8b3d1296286a60f4f35a0281a59c1a4105e
coderus <coderusinbox@gmail.com>
2021-01-14 15:56:15 +0300

    [server] Support for cdn static files

--- src/LampWebServer.cpp
+++ src/LampWebServer.cpp
@@ -437,11 +437,14 @@ LampWebServer::LampWebServer(uint16_t webPort)
 
     webServer->addHandler(socket);
 
+    webServer->rewrite(PSTR("/"), PSTR("/index.html"));
+    webServer->rewrite(PSTR("/index.html"), PSTR("/index-cdn.html")).setFilter(ON_STA_FILTER);
+    webServer->rewrite(PSTR("/wifi.html"), PSTR("/wifi-cdn.html")).setFilter(ON_STA_FILTER);
     webServer->serveStatic(PSTR("/static/js/"), FLASHFS, PSTR("/"), PSTR("max-age=86400"));
     webServer->serveStatic(PSTR("/static/css/"), FLASHFS, PSTR("/"), PSTR("max-age=86400"));
     webServer->serveStatic(PSTR("/effects.json"), FLASHFS, PSTR("/effects.json"), PSTR("no-cache"));
     webServer->serveStatic(PSTR("/settings.json"), FLASHFS, PSTR("/settings.json"), PSTR("no-cache"));
-    webServer->serveStatic(PSTR("/"), FLASHFS, PSTR("/"), PSTR("max-age=86400")).setDefaultFile(PSTR("index.html"));
+    webServer->serveStatic(PSTR("/"), FLASHFS, PSTR("/"), PSTR("max-age=86400"));
 
     webServer->on(PSTR("/effectJson"), HTTP_GET, [](AsyncWebServerRequest *request) {
         PrettyAsyncJsonResponse *response = new PrettyAsyncJsonResponse(false, 1024);
